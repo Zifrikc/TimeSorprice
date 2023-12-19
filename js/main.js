e7234b274b4de506cc24c24d7df20cdeb3add94a
Zifrikc <81603223+zuran21@users.noreply.github.com>
2023-12-19 17:12:53 -0500

    changes 2

---
+++ js/main.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Establecer el tiempo en segundos
+    var tiempoRestante = 60; // Cambia este valor al tiempo deseado en segundos
+    
+    var contadorElemento = document.getElementById('contador');
+    var botonDescarga = document.getElementById('boton-descarga');
+
+    function actualizarContador() {
+        var minutos = Math.floor(tiempoRestante / 60);
+        var segundos = tiempoRestante % 60;
+        contadorElemento.textContent = `${minutos}:${segundos < 10 ? '0' : ''}${segundos}`;
+    }
+
+    function habilitarDescarga() {
+        botonDescarga.style.display = 'block';
+    }
+
+    function iniciarTemporizador() {
+        var temporizador = setInterval(function () {
+            tiempoRestante--;
+
+            if (tiempoRestante <= 0) {
+                clearInterval(temporizador);
+                habilitarDescarga();
+            }
+
+            actualizarContador();
+        }, 1000);
+    }
+
+    // Iniciar temporizador cuando la página se carga
+    iniciarTemporizador();
+});
